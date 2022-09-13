86848b77feea6c14fd3f163156a1970ad2ee854b
mayconblopes <lopes.maycon@gmail.com>
2022-09-13 20:56:50 -0300

    Changed and hided secret key

--- instituto_ls_project/settings.py
+++ instituto_ls_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n@l)79%hh#2mwkvv!xop)du^!u01hr=uk*+ee!)$*a_hp*5su^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
